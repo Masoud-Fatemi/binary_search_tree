7036fb234232acf15cf28e063159a84bc8d3aa61
Masoud <masoud.fatemi1990@gmail.com>
2024-01-05 12:41:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 09:48:11 2024
+
+@author: masoud
+"""
+
+class Node:
+    
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+            
+    def __str__(self):
+        return str(self.value)
+    
+class BST:
+    
+    def __init__(self):
+        self.root = None
+        
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
+                
+    def _insert_recursive(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert_recursive(node.left, value)
+        elif value > node.value:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert_recursive(node.right, value)
+                
+    def search(self, value):
+        return self._search_recursive(self.root, value)
+    
+    def _search_recursive(self, node, value):
+        if node is None or node.value == value:
+            return node
+        if value < node.value:
+            return self._search_recursive(node.left, value)
+        else:
+            return self._search_recursive(node.right, value)
+
+bst = BST()
+
+bst.insert(5)
+bst.insert(3)
+bst.insert(7)
+bst.insert(2)
+bst.insert(4)
+bst.insert(6)
+bst.insert(8)
+
+print("Searching for elements:")
+print(bst.search(4))  # Found, returns the node (4)
+print(bst.search(9))  # Not found, returns None 
